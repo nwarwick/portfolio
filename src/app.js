0b07334e2d06f150e1eda9f7e7a667357e746ae6
nwarwick <nicholaswarwick@icloud.com>
2018-04-11 21:52:25 -0700

    Add fontawesome

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 import './scss/app.scss';
 import './resume.pdf';
 import './img/favicon.ico';
+
+import fontawesome from '@fortawesome/fontawesome'
+import faPhone from '@fortawesome/fontawesome-free-solid/faPhone'
+import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope'
+fontawesome.library.add(faPhone, faEnvelope)

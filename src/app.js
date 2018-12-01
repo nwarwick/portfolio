c38563219d63417c0e2aa6affbc6373f063884fc
Nick Warwick <nicholaswarwick@icloud.com>
2018-12-01 13:31:52 -0500

    Port to nextjs

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import './scss/app.scss';
-import './nick-warwick-resume.pdf';
-import './img/favicon.ico';
-
-import fontawesome from '@fortawesome/fontawesome'
-import faPhone from '@fortawesome/fontawesome-free-solid/faPhone'
-import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope'
-fontawesome.library.add(faPhone, faEnvelope)

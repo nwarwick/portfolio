1e8229448d038834e57d5d441697e7cc314f0b49
Nick Warwick <nicholaswarwick@icloud.com>
2018-12-10 11:01:07 -0500

    Move project list to data file

---
+++ api/data.js
@@ -0,0 +1,65 @@
+
+   module.exports = [
+      {
+        id: '1',
+        title: 'Calcohol',
+        img: '/static/img/calcohol/calcohol-logo.png',
+        description:
+          'Calcohol is an application that lets users search for liquor and view a calculated alcohol per dollar (APD) score.'
+      },
+      {
+        id: '2',
+        title: 'Earthquake Data Visualizer',
+        img: '/static/img/earthquake-visualizer/earthquake-visualizer.png',
+        description: 'An earthquake data visualization application.'
+      },
+      {
+        id: '3',
+        title: 'ScheduleX',
+        img: '/static/img/schedulex/schedulex.png',
+        description: 'Displays a list of upcoming SpaceX launches.'
+      },
+      {
+        id: '4',
+        title: 'Kozy',
+        img: '/static/img/kozy/kozy-logo.png',
+        description: 'An ambient sound web app.'
+      },
+      {
+        id: '5',
+        title: 'Custom Website',
+        img: '/static/img/custom-website/code.jpg',
+        description: 'This is a custom website I built from scratch for a client.'
+      },
+      {
+        id: '6',
+        title: 'PaceTheMusic',
+        img: '/static/img/ptm/ptm.png',
+        description:
+          'A program that generates music playlists based on user specified tempos.'
+      },
+      {
+        id: '7',
+        title: 'DREAMR',
+        img: '/static/img/dreamr/logo.png',
+        description: 'A Mixed Reality risk/data evaluation and analyisis program.'
+      },
+      {
+        id: '8',
+        title: 'Chomp',
+        img: '/static/img/chomp/chomp-logo.png',
+        description: 'A 2D survival game.'
+      },
+      {
+        id: '9',
+        title: 'Panzer',
+        img: '/static/img/panzer/panzer.png',
+        description: 'A 2.5D survival game.'
+      },
+      {
+        id: '10',
+        title: 'Kitten Ninja',
+        img: '/static/img/kitten-ninja/kitten-ninja.png',
+        description: 'A 2D side scrolling game.'
+      }
+    ]

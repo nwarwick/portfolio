b145be12b97d4f7a5050ffbd6e65b3d767984595
Nick Warwick <nicholaswarwick@icloud.com>
2018-12-10 10:59:17 -0500

    Use link component

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,28 @@
+import Link from 'next/link'
+
 const Nav = props => (
-  <nav className="navbar">
-    <h1>{props.pageTitle}</h1>
-    <div>
-      <a href="/">Home</a>
-      <a href="/projects">Projects</a>
-      <a href="/about">About</a>
-      <a href="/resume">Resume</a>
-      <a href="/contact">Contact</a>
-    </div>
-  </nav>
+  <header>
+    <nav className='navbar'>
+      <h1>{props.navTitle}</h1>
+      <div className='navbar-links'>
+        <Link prefetch href='/'>
+          <a>Home</a>
+        </Link>
+        <Link prefetch href='/projects'>
+          <a>Projects</a>
+        </Link>
+        <Link prefetch href='/about'>
+          <a>About</a>
+        </Link>
+        <Link prefetch href='/static/nick-warwick-resume.pdf'>
+          <a target='_blank'>Resume</a>
+        </Link>
+        <Link prefetch href='/contact'>
+          <a>Contact</a>
+        </Link>
+      </div>
+    </nav>
+  </header>
 )
 
 export default Nav

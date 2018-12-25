426c09365dacd99a3272e52ca7f60b2678fcf0aa
Nick Warwick <nicholaswarwick@icloud.com>
2018-12-24 23:02:34 -0800

    Update project info

--- api/data.js
+++ api/data.js
@@ -1,65 +1,116 @@
-
-   module.exports = [
-      {
-        id: '1',
-        title: 'Calcohol',
-        img: '/static/img/calcohol/calcohol-logo.png',
-        description:
-          'Calcohol is an application that lets users search for liquor and view a calculated alcohol per dollar (APD) score.'
-      },
-      {
-        id: '2',
-        title: 'Earthquake Data Visualizer',
-        img: '/static/img/earthquake-visualizer/earthquake-visualizer.png',
-        description: 'An earthquake data visualization application.'
-      },
-      {
-        id: '3',
-        title: 'ScheduleX',
-        img: '/static/img/schedulex/schedulex.png',
-        description: 'Displays a list of upcoming SpaceX launches.'
-      },
-      {
-        id: '4',
-        title: 'Kozy',
-        img: '/static/img/kozy/kozy-logo.png',
-        description: 'An ambient sound web app.'
-      },
-      {
-        id: '5',
-        title: 'Custom Website',
-        img: '/static/img/custom-website/code.jpg',
-        description: 'This is a custom website I built from scratch for a client.'
-      },
-      {
-        id: '6',
-        title: 'PaceTheMusic',
-        img: '/static/img/ptm/ptm.png',
-        description:
-          'A program that generates music playlists based on user specified tempos.'
-      },
-      {
-        id: '7',
-        title: 'DREAMR',
-        img: '/static/img/dreamr/logo.png',
-        description: 'A Mixed Reality risk/data evaluation and analyisis program.'
-      },
-      {
-        id: '8',
-        title: 'Chomp',
-        img: '/static/img/chomp/chomp-logo.png',
-        description: 'A 2D survival game.'
-      },
-      {
-        id: '9',
-        title: 'Panzer',
-        img: '/static/img/panzer/panzer.png',
-        description: 'A 2.5D survival game.'
-      },
-      {
-        id: '10',
-        title: 'Kitten Ninja',
-        img: '/static/img/kitten-ninja/kitten-ninja.png',
-        description: 'A 2D side scrolling game.'
-      }
-    ]
+module.exports = [
+  {
+    id: '1',
+    title: 'Calcohol',
+    previewImg: '/static/img/calcohol/calcohol-logo.png',
+    previewDescription:
+      'Calcohol is an application that lets users search for liquor and view a calculated alcohol per dollar (APD) score.',
+    fullImg: '/static/img/calcohol/calcohol.png',
+    fullDescription:
+      `<p>Calcohol is an application that lets users search for liquor and view a scaled alcohol per dollar (APD) score, allowing them to be as economic as possible when it comes to drinking. Very helpful for college students!</p><p>The data was previously pulled from a government API, but I moved it into my own database and wrote my own API in order to make it more efficient and improve the search functionality.</p>`,
+    technology: ['Vue.js', 'Node.js', 'Express', 'MongoDB'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '2',
+    title: 'Earthquake Data Visualizer',
+    previewImg: '/static/img/earthquake-visualizer/earthquake-visualizer.png',
+    previewDescription: 'An earthquake data visualization application.',
+    fullImg: '/static/img/earthquake-visualizer/earthquake-visualizer2.png',
+    fullDescription:
+      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
+    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '3',
+    title: 'ScheduleX',
+    previewImg: '/static/img/schedulex/schedulex.png',
+    previewDescription: 'Displays a list of upcoming SpaceX launches.',
+    fullImg: '/static/img/schedulex/schedulex.png',
+    fullDescription:
+      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
+    technology: ['Javascript', 'HTML', 'CSS', 'Parcel'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '4',
+    title: 'Kozy',
+    previewImg: '/static/img/kozy/kozy-logo.png',
+    previewDescription: 'An ambient sound web app.',
+    fullImg: '/static/img/kozy/kozy.png',
+    fullDescription:
+      'Kozy is a simple, lightweight, ambient sound web app written using vanilla Javascript. I created it to practice my web development and design skills.',
+    technology: ['Javascript', 'HTML', 'SCSS', 'Parcel', 'Firebase'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '5',
+    title: 'Custom Website',
+    previewImg: '/static/img/custom-website/code.jpg',
+    previewDescription:
+      'This is a custom website I built from scratch for a client.',
+    fullImg: '/static/img/custom-website/custom-website.png',
+    fullDescription:
+      'This is an example of a custom website I built from scratch for a client. It is fully responsive and works on phones and tablets.',
+    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '6',
+    title: 'PaceTheMusic',
+    previewImg: '/static/img/ptm/ptm.png',
+    previewDescription:
+      'A program that generates music playlists based on user specified tempos.',
+    fullImg: '/static/img/ptm/ptm2.png',
+    fullDescription:
+      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
+    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '7',
+    title: 'DREAMR',
+    previewImg: '/static/img/dreamr/logo.png',
+    previewDescription:
+      'A Mixed Reality risk/data evaluation and analyisis program.',
+    fullImg: '/static/img/dreamr/dreamr.png',
+    fullDescription:
+      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
+    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '8',
+    title: 'Chomp',
+    previewImg: '/static/img/chomp/chomp-logo.png',
+    previewDescription: 'A 2D survival game.',
+    fullImg: '/static/img/chomp/chomp.png',
+    fullDescription:
+      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
+    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '9',
+    title: 'Panzer',
+    previewImg: '/static/img/panzer/panzer.png',
+    previewDescription: 'A 2.5D survival game.',
+    fullImg: '/static/img/panzer/panzer2.png',
+    fullDescription:
+      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
+    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  },
+  {
+    id: '10',
+    title: 'Kitten Ninja',
+    previewImg: '/static/img/kitten-ninja/kitten-ninja.png',
+    previewDescription: 'A 2D side scrolling game.',
+    fullImg: '/static/img/kitten-ninja/kitten-ninja2.png',
+    fullDescription:
+      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
+    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
+    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+  }
+]

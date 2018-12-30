d596cf9374e6e8f18a2b3a7c26a6c90b543dfcba
Nick Warwick <nicholaswarwick@icloud.com>
2018-12-30 11:19:13 -0800

    Add project page view

---
+++ pages/project-page.js
@@ -0,0 +1,55 @@
+import Layout from '../components/Layout'
+import projects from '../api/data'
+
+export default class ProjectPage extends React.PureComponent {
+  static async getInitialProps ({ query, res }) {
+    const project = projects.find(project => project.id === query.slug)
+
+    if (!project && res) {
+      res.statusCode = 404
+    }
+
+    return { project }
+  }
+
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    const {title, fullDescription, technology, url, fullImg} = this.props.project
+
+    return (
+      <Layout
+        pageTitle='Nick Warwick | Freelance Web Developer and Designer'
+        metaContent='My name is Nick Warwick. I am a fullstack web developer and designer living in Victoria British Columbia, Canada. I am available for freelance work. I can build and design websites and web applications as well as apply SEO standards.'
+      >
+        {this.props.project ? (
+          <div className='project-page'>
+            <div className='grid-container'>
+              <div className='project'>
+                <h1>{title}</h1>
+                <h2>What is it?</h2>
+                <p>{fullDescription}</p>
+                <h2>What was used to make it?</h2>
+                <ul>
+                  {technology.map((item, id) => {
+                    return <li key={id}>{item}</li>
+                  })}
+                </ul>
+                <h2>Where is it?</h2>
+                <p>{url}</p>
+                <button>Back</button>
+              </div>
+              <div className='project-image'>
+                  <img src={fullImg} alt="Project screen shot"/>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className='project-page-sorry'>Could not find project :(</div>
+        )}
+      </Layout>
+    )
+  }
+}

858311fb2b436c2a0eb1aff303ea1ec12f97d5aa
Nick Warwick <nicholaswarwick@icloud.com>
2019-01-01 16:07:56 -0800

    Formatting

--- components/ProjectPreview.js
+++ components/ProjectPreview.js
@@ -3,19 +3,18 @@
 import { Link, Router } from '../routes'
 
 const ProjectPreview = props => {
-  const {previewImg, title, previewDescription, id} = props.project
+  const { previewImg, title, previewDescription, id } = props.project
 
   return (
-  <div className='project-preview'>
-    <img className='project-preview-img' src={previewImg} alt='Project' />
-    <h2>{title}</h2>
-    <p>{previewDescription}</p>
-    <Link route='project-page' params={{ slug: id }}>
-      <a className='test'>
-        Learn More
-      </a>
-    </Link>
-  </div>)
+    <div className='project-preview'>
+      <img className='project-preview-img' src={previewImg} alt='Project' />
+      <h2>{title}</h2>
+      <p>{previewDescription}</p>
+      <Link route='project-page' params={{ slug: id }}>
+        <a className='test'>Learn More</a>
+      </Link>
+    </div>
+  )
 }
 
 export default ProjectPreview

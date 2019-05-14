b45ac9520ea0cae93c2eedb1325596754c1c2100
Nick Warwick <nicholaswarwick@icloud.com>
2019-05-14 11:32:02 -0400

    Update data

--- static/js/data.js
+++ static/js/data.js
@@ -9,7 +9,7 @@ module.exports = [
     fullDescription:
       `<p>Calcohol is an application that lets users search for liquor and view a scaled alcohol per dollar (APD) score, allowing them to be as economic as possible when it comes to drinking. Very helpful for college students!</p><p>The data was previously pulled from a government API, but I moved it into my own database and wrote my own API in order to make it more efficient and improve the search functionality.</p>`,
     technology: ['Vue.js', 'Node.js', 'Express', 'MongoDB'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+    url: 'https://github.com/nwarwick/calcohol'
   },
   {
     id: '2',
@@ -18,8 +18,8 @@ module.exports = [
     previewDescription: 'An earthquake data visualization application.',
     fullImg: '/static/img/earthquake-visualizer/earthquake-visualizer2.png',
     fullDescription:
-      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
-    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
+      '<p>A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.</p>',
+    technology: ['JavaScript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
     url: 'https://github.com/nwarwick/earthquake-data-visualizer'
   },
   {
@@ -30,8 +30,8 @@ module.exports = [
     fullImg: '/static/img/schedulex/schedulex.png',
     fullDescription:
       'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
-    technology: ['Javascript', 'HTML', 'CSS', 'Parcel'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+    technology: ['JavaScript', 'HTML', 'CSS', 'Parcel'],
+    url: 'https://github.com/nwarwick/schedule-x'
   },
   {
     id: '4',
@@ -40,9 +40,9 @@ module.exports = [
     previewDescription: 'An ambient sound web app.',
     fullImg: '/static/img/kozy/kozy.png',
     fullDescription:
-      'Kozy is a simple, lightweight, ambient sound web app written using vanilla Javascript. I created it to practice my web development and design skills.',
-    technology: ['Javascript', 'HTML', 'SCSS', 'Parcel', 'Firebase'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+      '<p>Kozy is a simple, lightweight, ambient sound web app written using vanilla JavaScript. I created it to practice my web development and design skills.</p>',
+    technology: ['JavaScript', 'HTML', 'SCSS', 'Parcel', 'Firebase'],
+    url: 'https://github.com/nwarwick/kozy'
   },
   {
     id: '5',
@@ -52,9 +52,9 @@ module.exports = [
       'This is a custom website I built from scratch for a client.',
     fullImg: '/static/img/custom-website/custom-website.png',
     fullDescription:
-      'This is an example of a custom website I built from scratch for a client. It is fully responsive and works on phones and tablets.',
-    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+      '<p>This is an example of a custom website I built from scratch for a client. It is fully responsive and works on phones and tablets.</p>',
+    technology: ['JavaScript', 'HTML', 'CSS', 'Firebase'],
+    url: 'https://kassandra-website.firebaseapp.com/'
   },
   {
     id: '6',
@@ -64,9 +64,9 @@ module.exports = [
       'A program that generates music playlists based on user specified tempos.',
     fullImg: '/static/img/ptm/ptm2.png',
     fullDescription:
-      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
-    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+      '<p>PaceTheMusic is a desktop application created by Kaileen McCulloch, James Gray, and myself. It allows users to create playlists based on tempos. If you want your playlist to start off with slower music then ramp up to fast music, simply specify your tempos and the application will do the rest! I worked on the front end of the application and tied it together with the backend.</p>',
+    technology: ['Python', 'SQLAlchemy', 'PyQt5'],
+    url: 'https://github.com/james-gray/pace-the-music'
   },
   {
     id: '7',
@@ -76,9 +76,9 @@ module.exports = [
       'A Mixed Reality risk/data evaluation and analyisis program.',
     fullImg: '/static/img/dreamr/dreamr.png',
     fullDescription:
-      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
-    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+      '<p>DREAMR (Data and Risk Evaluation and Analysis in Mixed Reality) is a data and risk evaluation tool that takes advantage of the Mixed Reality environment provided by the HoloLens, in order to help with data visualization.</p>',
+    technology: ['C#', 'Unity', 'HoloLens SDK'],
+    url: ''
   },
   {
     id: '8',
@@ -87,9 +87,9 @@ module.exports = [
     previewDescription: 'A 2D survival game.',
     fullImg: '/static/img/chomp/chomp.png',
     fullDescription:
-      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
-    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+      '<p>Chomp is a 2D surival game in which you play as a worm trying not to die from other worms that are trying to kill you for some reason. The world is filled with powerups and other items that can aid you in your quest to not die. The game was built by William Tsease and myself.</p>',
+    technology: ['C#', 'Unity'],
+    url: 'https://github.com/nwarwick/chomp'
   },
   {
     id: '9',
@@ -98,9 +98,9 @@ module.exports = [
     previewDescription: 'A 2.5D survival game.',
     fullImg: '/static/img/panzer/panzer2.png',
     fullDescription:
-      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
-    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+      '<p>Panzer is a 2.5D survival game in which you play as a sentient tank fighting for survival in a world filled with other sentient tanks.</p>',
+    technology: ['C#', 'Unity'],
+    url: 'https://github.com/nwarwick/panzer'
   },
   {
     id: '10',
@@ -109,8 +109,8 @@ module.exports = [
     previewDescription: 'A 2D side scrolling game.',
     fullImg: '/static/img/kitten-ninja/kitten-ninja2.png',
     fullDescription:
-      'A simple earthquake data visualization application that displays earthquakes from the last 24 hours and encodes their location and magnitude as the location and radius of a circle.',
-    technology: ['Javascript', 'HTML', 'CSS', 'Mapbox GL', 'Firebase'],
-    url: 'https://github.com/nwarwick/earthquake-data-visualizer'
+      '<p>Kitten Ninja is a simple game about a kitten who also happens to be a ninja. The game was created for a class assignment and may be expanded upon in the future.</p>',
+    technology: ['C#', 'Unity'],
+    url: 'https://github.com/nwarwick/kitten-ninja'
   }
 ]

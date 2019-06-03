f47b35f8da7f837ae68c175846d6a8270c20e7da
Nick Warwick <nicholaswarwick@icloud.com>
2019-06-03 13:55:02 -0400

    Update location

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const Index = () => (
   <Layout
     pageTitle='Nick Warwick | Freelance Web Developer and Designer'
-    metaContent='My name is Nick Warwick. I am a fullstack web developer and designer living in Victoria British Columbia, Canada. I am available for freelance work. I can build and design websites and web applications as well as apply SEO standards.'
+    metaContent='My name is Nick Warwick. I am a fullstack web developer and designer living in Montreal Canada. I am available for freelance work. I can build and design websites and web applications as well as apply SEO standards.'
     navTitle='Nick Warwick'
   >
     <div className='index-page'>

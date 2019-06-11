88e7f1d8e270cc83af845b0123dc27a6edea557d
Nick Warwick <nicholaswarwick@icloud.com>
2019-06-11 16:07:16 -0400

    Attempt to remove trailing slashes from URL

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withSass = require('@zeit/next-sass')
 const projects = require('./static/js/data')
 
 module.exports = withSass({
+  experimental: {
+    exportTrailingSlash: false
+  },
   exportPathMap: function() {
     path = {
       '/': { page: '/' },

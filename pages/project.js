ea9e83cac226d7ea2ef7b468abdfcacac635a294
Nick Warwick <nicholaswarwick@icloud.com>
2019-08-01 15:27:21 -0400

    Convert to functional component

--- pages/project.js
+++ pages/project.js
@@ -1,59 +1,56 @@
 import Layout from '../components/Layout'
 
-export default class Project extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const Project = props => {
+  const {
+    title,
+    fullDescription,
+    metaDescription,
+    technology,
+    url,
+    fullImg
+  } = props.url.query
 
-  render() {
-    const {
-      title,
-      fullDescription,
-      metaDescription,
-      technology,
-      url,
-      fullImg
-    } = this.props.url.query
-
-    return (
-      <Layout
-        pageTitle={`Projects - ${title}`}
-        metaContent={metaDescription}
-        canonicalUrl={`https://nwarwick.info/projects/${title
-          .trim()
-          .toLowerCase()
-          .replace(/[\s]+/g, '-')}`}
-      >
-        <div className='project-page'>
-          <div className='grid-container'>
-            <div className='project'>
-              <h1 className='project-title'>{title}</h1>
-              <h2>What is it?</h2>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: fullDescription
-                }}
-              />
-              <h2>What was used to make it?</h2>
-              <ul>
-                {technology.map((item, id) => {
-                  return <li key={id}>{item}</li>
-                })}
-              </ul>
-              <h2>Where is it?</h2>
-              <a href={url} target='_blank'>
-                {url}
-              </a>
-              <a className='btn' href='/projects'>
-                Back
-              </a>
-            </div>
-            <div className='project-image'>
-              <img src={fullImg} alt='Project screen shot' />
-            </div>
+  return (
+    <Layout
+      pageTitle={`Projects - ${title}`}
+      metaContent={metaDescription}
+      canonicalUrl={`https://nwarwick.info/projects/${title
+        .trim()
+        .toLowerCase()
+        .replace(/[\s]+/g, '-')}`}
+    >
+      <div className='project-page'>
+        <div className='grid-container'>
+          <div className='project'>
+            <h1 className='project-title'>{title}</h1>
+            <h2>What is it?</h2>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: fullDescription
+              }}
+            />
+            <h2>What was used to make it?</h2>
+            <ul>
+              {technology.map((item, id) => {
+                return <li key={id}>{item}</li>
+              })}
+            </ul>
+            <h2>Where is it?</h2>
+            <a href={url} target='_blank'>
+              {url}
+            </a>
+            <a className='btn' href='/projects'>
+              Back
+            </a>
+          </div>
+          <div className='project-image'>
+            <img src={fullImg} alt='Project screen shot' />
           </div>
         </div>
-      </Layout>
-    )
-  }
+      </div>
+    </Layout>
+  )
 }
+
+export default Project
+

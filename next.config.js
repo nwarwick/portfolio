30f7709b6283dad6283118111505c46ef1b6a988
Nick Warwick <nicholaswarwick@icloud.com>
2019-09-08 10:15:00 -0400

    Update config

--- next.config.js
+++ next.config.js
@@ -2,15 +2,26 @@ const withSass = require('@zeit/next-sass')
 const projects = require('./static/js/data')
 
 module.exports = withSass({
-  experimental: {
-    exportTrailingSlash: false
-  },
-  async function(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    return {
+  // experimental: {
+  //   exportTrailingSlash: false
+  // },
+  async exportPathMap() {
+    const pages = projects.reduce(
+      (pages, project) =>
+        Object.assign({}, pages, {
+          [`/projects/${project.slug}`]: {
+            page: '/project',
+            query: { slug: project.slug }
+          }
+        }),
+      {}
+    )
+
+    return Object.assign({}, pages, {
       '/': { page: '/' },
       '/about': { page: '/about' },
       '/projects': { page: '/projects' },
       '/contact': { page: '/contact' }
-    }
+    })
   }
 })

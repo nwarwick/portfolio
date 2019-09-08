23ca8d41bdc57acbc9d0b7a9b78efd18896456d6
Nick Warwick <nicholaswarwick@icloud.com>
2019-09-08 10:23:28 -0400

    Clean up JSX

--- pages/project.js
+++ pages/project.js
@@ -8,44 +8,52 @@ export default class extends Component {
     const project = data.find(project => {
       return project.slug === query.slug
     })
-    return project
+    return { ...project }
   }
 
   render() {
-    const { project } = { ...this.props }
+    const {
+      title,
+      metaDescription,
+      slug,
+      location,
+      fullImg,
+      technology,
+      fullDescription
+    } = { ...this.props }
 
     return (
       <Layout
-        pageTitle={`Projects - ${this.props.title}`}
-        metaContent={this.props.metaDescription}
-        canonicalUrl={`https://nwarwick.info/projects/${this.props.slug}`}
+        pageTitle={`Projects - ${title}`}
+        metaContent={metaDescription}
+        canonicalUrl={`https://nwarwick.info/projects/${slug}`}
       >
         <div className='project-page'>
           <div className='grid-container'>
             <div className='project'>
-              <h1 className='project-title'>{this.props.title}</h1>
+              <h1 className='project-title'>{title}</h1>
               <h2>What is it?</h2>
               <div
                 dangerouslySetInnerHTML={{
-                  __html: this.props.fullDescription
+                  __html: fullDescription
                 }}
               />
               <h2>What was used to make it?</h2>
               <ul>
-                {this.props.technology.map((item, id) => {
+                {technology.map((item, id) => {
                   return <li key={id}>{item}</li>
                 })}
               </ul>
               <h2>Where is it?</h2>
-              <a href={this.props.location} target='_blank'>
-                {this.props.location}
+              <a href={location} target='_blank'>
+                {location}
               </a>
               <a className='btn' href='/projects'>
                 Back
               </a>
             </div>
             <div className='project-image'>
-              <img src={this.props.fullImg} alt='Project screen shot' />
+              <img src={fullImg} alt='Project screen shot' />
             </div>
           </div>
         </div>

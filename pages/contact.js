249c10fe31160f1a7bd6ed0a63a8ad93334857dd
Nick Warwick <nicholaswarwick@icloud.com>
2020-05-08 13:30:43 -0400

    Update meta content

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const Contact = () => (
   <Layout
     pageTitle='Nick Warwick | Contact Me'
-    metaContent='I am always open to talk. Give me a call or send me an email!'
+    metaContent='I am always open to talk. Feel free to send me an email to schedule a chat!'
     navTitle='Contact Me'
     canonicalUrl='https://nwarwick.info/contact'
   >

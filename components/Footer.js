53591075a421a06fe1122322eee4e7b6e858645b
Nick Warwick <nicholaswarwick@icloud.com>
2023-03-02 11:55:11 -0500

    A new beginning
    Rewriting the site in Bridgetown. This is the first commit that removes
    the old code and adds the new Bridgetown structure.

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = props => (
-  <footer className='footer'>
-    <span>Who needs a footer for a portfolio?</span>
-  </footer>
-)
-
-export default Footer
---
+++ frontend/javascript/index.js
@@ -0,0 +1,8 @@
+import "index.scss"
+// import "_projects.scss"
+import "syntax-highlighting.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+
+console.info("Bridgetown is loaded!")
